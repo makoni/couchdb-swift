9389776664e4ce35bb303070eee3b2872c385f86
Sergey Armodin <dolph-in@yandex.ru>
2025-07-27 03:58:55 +0700

    updated tests

--- Tests/CouchDBClientTests/AttachmentsAPI.tests.swift
+++ Tests/CouchDBClientTests/AttachmentsAPI.tests.swift
@@ -19,8 +19,15 @@ fileprivate let testsDB = "attachments_api_tests"
 struct AttachmentsAPITests {
 	let testDocId = "test_doc_with_attachment"
 	let testAttachmentName = "test-image.png"
-	let testAttachmentPath = "Tests/CouchDBClientTests/test-image.png"
 	let testContentType = "image/png"
+	// Embedded PNG data (1x1 transparent pixel)
+	let embeddedTestImageData: Data = Data([
+		0x89,0x50,0x4E,0x47,0x0D,0x0A,0x1A,0x0A,0x00,0x00,0x00,0x0D,0x49,0x48,0x44,0x52,
+		0x00,0x00,0x00,0x01,0x00,0x00,0x00,0x01,0x08,0x06,0x00,0x00,0x00,0x1F,0x15,0xC4,
+		0x89,0x00,0x00,0x00,0x0A,0x49,0x44,0x41,0x54,0x78,0x9C,0x63,0x00,0x01,0x00,0x00,
+		0x05,0x00,0x01,0x0D,0x0A,0x2D,0xB4,0x00,0x00,0x00,0x00,0x49,0x45,0x4E,0x44,0xAE,
+		0x42,0x60,0x82
+	])
 
 	@Test("Setup: Create DB and insert test doc")
 	func setupDBAndDoc() async throws {
@@ -39,7 +46,7 @@ struct AttachmentsAPITests {
 
 	@Test("Upload attachment to document")
 	func uploadAttachment() async throws {
-		let data = try Data(contentsOf: URL(fileURLWithPath: testAttachmentPath))
+		let data = embeddedTestImageData
 		let response = try await couchDBClient.get(fromDB: testsDB, uri: testDocId)
 		let expectedBytes = response.headers.first(name: "content-length").flatMap(Int.init) ?? 1024 * 1024 * 10
 		var bytes = try await response.body.collect(upTo: expectedBytes)
@@ -60,7 +67,7 @@ struct AttachmentsAPITests {
 
 	@Test("Download attachment and verify data matches upload")
 	func downloadAttachment() async throws {
-		let uploadedData = try Data(contentsOf: URL(fileURLWithPath: testAttachmentPath))
+		let uploadedData = embeddedTestImageData
 		let downloadedData = try await couchDBClient.downloadAttachment(
 			dbName: testsDB,
 			docId: testDocId,
